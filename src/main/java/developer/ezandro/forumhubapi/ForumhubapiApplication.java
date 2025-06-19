2dc241a79f0ae7d57928d75684edd3e2ca0f14ee
ezbueno <ezandrobueno@yahoo.com.br>
2025-06-19 00:04:25 -0300

    initial commit

---
+++ src/main/java/developer/ezandro/forumhubapi/ForumhubapiApplication.java
@@ -0,0 +1,11 @@
+package developer.ezandro.forumhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ForumhubapiApplication.class, args);
+	}
+}
---
+++ src/test/java/developer/ezandro/forumhubapi/ForumhubapiApplicationTests.java
@@ -0,0 +1,13 @@
+package developer.ezandro.forumhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
