10f2c188e785f65b297da2ab7560928beb2c1f14
ezbueno <ezandrobueno@yahoo.com.br>
2025-06-19 01:09:09 -0300

    feat: Dotenv config

--- src/main/java/developer/ezandro/forumhubapi/ForumhubapiApplication.java
+++ src/main/java/developer/ezandro/forumhubapi/ForumhubapiApplication.java
@@ -1,11 +1,18 @@
 package developer.ezandro.forumhubapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ForumhubapiApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+		dotenv.entries().forEach(e ->
+				System.setProperty(e.getKey(), e.getValue())
+		);
 		SpringApplication.run(ForumhubapiApplication.class, args);
 	}
 }
