7547cfa309ef4d676fbe627ce2c68658ff0509c4
ezbueno <ezandrobueno@yahoo.com.br>
2025-06-21 15:11:19 -0300

    refactor(UserRepository): remove unused findByName method
    Removed the unused findByName method that wasn't being called anywhere in the codebase.
    This cleanup helps maintain leaner code focused only on required features,
    reducing unnecessary complexity.

--- src/main/java/developer/ezandro/forumhubapi/repository/UserRepository.java
+++ src/main/java/developer/ezandro/forumhubapi/repository/UserRepository.java
@@ -2,12 +2,9 @@ package developer.ezandro.forumhubapi.repository;
 
 import developer.ezandro.forumhubapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByName(String name);
-
     Optional<User> findByEmail(String username);
 }
